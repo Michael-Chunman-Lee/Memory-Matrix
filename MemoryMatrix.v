f3b24b7130377f163d7d53b8a9c391b4b22cbdf9
Michael-Chunman-Lee <michaelchunman.lee@mail.utoronto.ca>
2019-03-16 21:48:23 -0400

    Added guess checking to memory matrix

--- MemoryMatrix.v
+++ MemoryMatrix.v
@@ -1,4 +1,8 @@
+//TODO: (for next milestone) Create FSM to control when to check guesses remaining and when to check for correct guesses
+
+// Main module for the memory matrix game
 module MemoryMatrix(
+	input [9:0] SW, 
 	input [3:0] KEY,
 	input CLOCK_50);
 	
@@ -6,7 +10,7 @@ module MemoryMatrix(
 	assign resetn = KEY[0];
 	assign start = ~KEY[1];
 	
-	wire [x:0] board;
+	wire [x:0] board; //TODO: determine size of board
 	
 	Board b0(
 	.start(start),
@@ -15,3 +19,38 @@ module MemoryMatrix(
 	.board(board));
 	
 endmodule
+
+module GuessRemaining(
+	input [x:0] input_guesses,
+	input clk,
+	input reset,
+	input enable,
+	output reg [x:0] remaining_guesses);	//TODO: determine number of guesses
+	
+	always @(posedge clk) begin
+		if (!reset)
+			remaining_guesses <= input_guesses;
+		else begin
+			if (enable) 
+				remaining_guesses <= remaining_guesses - 1;
+		end
+	end
+	
+endmodule
+
+module CheckGuess(
+	input [x:0] guess,
+	input [x:0] board,
+	input enable,
+	input reset,
+	output reg iscorrect);
+	
+	always @(posedge clk) begin
+		if (!reset)
+			iscorrect <= 1'b0;
+		else if (enable
+			iscorrect <= ((guess & board) > 0) ? 1'b1 : 1'b0;
+	end
+	
+endmodule
+				
